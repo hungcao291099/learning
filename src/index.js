3d97e5f0556ba65af0dccb0bea0bce61a874c014
hungcao291099 <giahung1135@gmail.com>
2023-08-04 01:53:05 -0700

    boostrap

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ var morgan = require('morgan')
 const hbs  = require('express-handlebars');
 const app = express()
 const port = 3000
+
+app.use(express.static(path.join(__dirname, '/public')));
 // http logger
 app.use(morgan('combined'));
-
-app.engine('.hbs', hbs({extname: '.hbs'}));
+//handlebars
+app.engine('.hbs', hbs.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-app.set('views', './src/resources/views')
+app.set('views',  path.join(__dirname, 'resources/views'))
+
+//start
 app.get('/', (req, res) => {
   res.render('home')
 })
-app.get('/', (req, res) => {
+app.get('/news', (req, res) => {
   res.render('news')
 })
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
