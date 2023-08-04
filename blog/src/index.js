e8bb19255bc463f789670a58b3ee505426ba9b16
hungcao291099 <giahung1135@gmail.com>
2023-08-03 21:26:41 -0700

    upload

---
+++ blog/src/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const path = require('path')
+var morgan = require('morgan')
+const hbs  = require('express-handlebars');
+const app = express()
+const port = 3000
+// http logger
+app.use(morgan('combined'));
+
+app.engine('.hbs', hbs({extname: '.hbs'}));
+app.set('view engine', '.hbs');
+app.set('views', './src/resources/views')
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/', (req, res) => {
+  res.render('news')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
