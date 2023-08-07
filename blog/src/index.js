3d6bff0645a112912f2e2a1c27b96a7d78b0cc7e
hungcao291099 <giahung1135@gmail.com>
2023-08-07 02:57:34 -0700

    save

--- blog/src/index.js
+++ blog/src/index.js
@@ -5,6 +5,9 @@ const hbs  = require('express-handlebars');
 const app = express()
 const port = 3000
 
+const route =require('./routes');
+
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, '/public')));
 // http logger
 app.use(morgan('combined'));
@@ -13,17 +16,9 @@ app.engine('.hbs', hbs.engine({extname: '.hbs'}));
 app.set('view engine', '.hbs');
 app.set('views',  path.join(__dirname, 'resources/views'))
 
-//start
-app.get('/', (req, res) => {
-  res.render('home')
-})
-app.get('/news', (req, res) => {
-  res.render('news')
-})
-app.get('/search', (req, res) => {
-  console.log(req.query);
-  res.render('search')
-})
+
+//route init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
