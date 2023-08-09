4430c65f2d473b75152cd528d5c92ace681ce9e8
hungcao291099 <giahung1135@gmail.com>
2023-08-09 03:03:35 -0700

    crud

--- blog/src/app/controllers/models/Course.js
+++ blog/src/app/controllers/models/Course.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
+const slug = require('mongoose-slug-generator');
+mongoose.plugin(slug);
 const Course = new Schema({
     name:{type: String},
-    discription:{type: String},
+    description:{type: String},
+    videoID:{type: String},
     image:{type: String},
-    crateAt:{type: Date, default: Date.now()},
-    updateAt:{type: Date, default: Date.now()},
+    slug: { type: String, slug: 'name', unique: true}
+},{
+    timestamps: true
 })
 
 module.exports = mongoose.model('Course', Course); 
