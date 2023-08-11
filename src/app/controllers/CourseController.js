a2ce485d11ec92e1a488b72d4d3de890f6cca752
hungcao291099 <giahung1135@gmail.com>
2023-08-11 03:01:15 -0700

    11-8

--- src/app/controllers/CourseController.js
+++ src/app/controllers/CourseController.js
@@ -50,6 +50,24 @@ class CourseController{
         .then(()=> res.redirect('back'))
         .catch(next)
     }
+
+    handleFormActions(req, res, next){
+    switch (req.body.action) {
+        case 'delete':
+            Course.delete({_id : {$in: req.body.courseIDs}})
+            .then(()=> res.redirect('back'))
+            .catch(next)
+            break;
+        case 'restore':
+            Course.restore({ _id: {$in: req.body.courseIDs}})
+            .then(()=> res.redirect('back'))
+            .catch(next)
+            break;
+        default:
+            res.json({message: 'Invalid action'});
+    }
+
+    }
    
 
 }
